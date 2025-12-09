89c1054452b2c44271286255f8307a7696d40b29
konatenhamedm <konatenhamed@gmail.com>
2025-12-09 22:07:55 +0000

    save

--- src/Entity/PaiementFacture.php
+++ src/Entity/PaiementFacture.php
@@ -3,81 +3,17 @@
 namespace App\Entity;
 
 use App\Repository\PaiementFactureRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PaiementFactureRepository::class)]
-class PaiementFacture
+class PaiementFacture extends Paiement
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+   
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    private ?\DateTimeInterface $date = null;
-
-    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
-    private ?string $montant = null;
-
-    #[ORM\Column(length: 50, nullable: true)]
-    private ?string $modePaiement = null;
-
-    #[ORM\Column(length: 100, nullable: true)]
-    private ?string $reference = null;
-
-    #[ORM\ManyToOne(inversedBy: 'paiements')]
-    #[ORM\JoinColumn(nullable: false)]
+    #[ORM\ManyToOne(inversedBy: 'paiementFactures')]
     private ?Facture $facture = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getDate(): ?\DateTimeInterface
-    {
-        return $this->date;
-    }
-
-    public function setDate(\DateTimeInterface $date): static
-    {
-        $this->date = $date;
-        return $this;
-    }
-
-    public function getMontant(): ?string
-    {
-        return $this->montant;
-    }
-
-    public function setMontant(string $montant): static
-    {
-        $this->montant = $montant;
-        return $this;
-    }
-
-    public function getModePaiement(): ?string
-    {
-        return $this->modePaiement;
-    }
-
-    public function setModePaiement(?string $modePaiement): static
-    {
-        $this->modePaiement = $modePaiement;
-        return $this;
-    }
-
-    public function getReference(): ?string
-    {
-        return $this->reference;
-    }
-
-    public function setReference(?string $reference): static
-    {
-        $this->reference = $reference;
-        return $this;
-    }
+    
 
     public function getFacture(): ?Facture
     {
@@ -87,6 +23,7 @@ class PaiementFacture
     public function setFacture(?Facture $facture): static
     {
         $this->facture = $facture;
+
         return $this;
     }
 }
